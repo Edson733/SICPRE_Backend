b245978e7653c0d109a7933495ea997597d2ae02
Edson733 <20213tn034@utez.edu.mx>
2023-07-24 17:12:27 -0600

    Primeras preparaciones

---
+++ src/config/express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); //Importaciones
+
+const app = express(); //Instanciar server
+app.set("port", process.env.PORT || 3000);
+
+//Middlewares
+app.use(cors({origins: '*'})); //Permite recibir cualquier peticion con X origen
+app.use(express.json({limit: '50mb'})); //Permite recibir json en el body de la peticion hasta 50mb
+
+//Routes
+app.get("/", (request, response) => {
+    response.send("Bienvenido a backend SICPRE");
+});
+
+//Endpoints
+
+
+module.exports = {
+    app
+}; //{app : app}
