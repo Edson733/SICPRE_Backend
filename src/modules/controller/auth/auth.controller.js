766cc3ed5234138572bcfc13d68ac33c84ec7768
Edson733 <20213tn034@utez.edu.mx>
2023-08-19 21:42:09 -0600

    Session

--- src/modules/controller/auth/auth.controller.js
+++ src/modules/controller/auth/auth.controller.js
@@ -4,13 +4,17 @@ const {validateError} = require('../../../utils/functions');
 
 const signin = async(req, res = response) => {
     try {
+        // La sesion esta activa
         console.log(req.body);
         const {email_usr, password_usr} = req.body;
         const token = await login(email_usr, password_usr);
         console.log(token);
+        console.log("La sesión esta activa");
         res.status(200).json(token);
     } catch (err) {
+        // La sesion no esta activa
         console.log(err);
+        console.log("La sesión no esta activa");
         const message = validateError(err);
         res.status(400).json({message});
     }
